e47409165ba36b56b7ede48b234780bcf8cd2e23
Italo Nobre <italonobre@ads.fiponline.edu.br>
2022-12-22 21:12:30 -0300

    refactoring

--- src/main/java/br/com/testes/api/repository/UserRepository.java
+++ src/main/java/br/com/testes/api/repository/UserRepository.java
@@ -1,10 +1,8 @@
 package br.com.testes.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 
 import br.com.testes.api.domain.User;
-@Repository
+
 public interface UserRepository extends JpaRepository<User,Long>{
 }
