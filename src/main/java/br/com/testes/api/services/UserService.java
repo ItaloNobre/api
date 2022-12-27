b5ebb176f27a52fc6ac6abb4d0c34065be3a2a44
Italo Nobre <italonobre@ads.fiponline.edu.br>
2022-12-26 22:14:20 -0300

    adding method create

--- src/main/java/br/com/testes/api/services/UserService.java
+++ src/main/java/br/com/testes/api/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import br.com.testes.api.domain.User;
+import br.com.testes.api.domain.dto.UserDTO;
 
 @Service
 public interface UserService {
@@ -12,4 +13,6 @@ public interface UserService {
     User findById(Long id);
     
     List<User> findAll();
+
+    User create(UserDTO obj);
 }
