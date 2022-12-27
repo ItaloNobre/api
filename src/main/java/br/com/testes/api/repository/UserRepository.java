c2dc893b321382c422819b67ad18a3471e5553c7
Italo Nobre <italonobre@ads.fiponline.edu.br>
2022-12-26 22:23:06 -0300

    exception for registered email

--- src/main/java/br/com/testes/api/repository/UserRepository.java
+++ src/main/java/br/com/testes/api/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package br.com.testes.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.testes.api.domain.User;
 
 public interface UserRepository extends JpaRepository<User,Long>{
+
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/br/com/testes/api/services/impl/UserServiceImplementacao.java
+++ src/main/java/br/com/testes/api/services/impl/UserServiceImplementacao.java
@@ -3,12 +3,15 @@ package br.com.testes.api.services.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.testes.api.domain.User;
+import br.com.testes.api.domain.dto.UserDTO;
 import br.com.testes.api.repository.UserRepository;
 import br.com.testes.api.services.UserService;
+import br.com.testes.api.services.exceptions.DataIntegratyViolationException;
 import br.com.testes.api.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,6 +19,9 @@ public class UserServiceImplementacao implements UserService {
 
     @Autowired
     private UserRepository repository;
+
+    @Autowired
+    private ModelMapper mapper;
 
     @Override
     public User findById(Long id) {
@@ -26,6 +32,20 @@ public class UserServiceImplementacao implements UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        findByEmail(obj);
+        return repository.save(mapper.map(obj, User.class));
+        
+    }
+
+    private void findByEmail(UserDTO obj){
+        Optional<User> user = repository.findByEmail(obj.getEmail());
+        if(user.isPresent()){
+            throw new DataIntegratyViolationException("E-mail já cadastrado no sistema");
+        }
     }
     
 }
